4628ce5d67bf07889bac96a78fc9268eca1d1a06
Mahdi-Massahi <Mahdi.Massahi@gmail.com>
2023-02-11 16:58:17 +0330

    recursion implemented some bugs got fixed but some left

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ driver = webdriver.Chrome(
 LIMIT_BY_TRANSFER_COUNT = 300
 UNHIDE_SMALL_TRANSACTIONS = True
 MIN_TRANSFER_AMOUNT = 0.0001
+MAX_SCAN_DEPTH = 2
 
 
 class Transfer:
@@ -51,64 +52,8 @@ class Transfer:
         self.is_outgoing = is_outgoing
 
 
-starting_address = (
-    "TTVQUuYxDZCnFcmju8k8zNJEaBWiR1z6tM"  # input("Enter the starting tron address: ")
-)
-
-
 def get_transfers_url(address: str) -> str:
     return f"https://tronscan.org/#/address/{address}/transfers"
-
-
-def get_address_from(content: str) -> str:
-    address = (
-        content.find_element(
-            By.XPATH,
-            "//td[@class='ant-table-cell ant_table address_max_width from_address']",
-        )
-        .find_element(By.XPATH, "//div[@class='ellipsis_box_start']")
-        .text
-        + content.find_element(
-            By.XPATH,
-            "//td[@class='ant-table-cell ant_table address_max_width from_address']",
-        )
-        .find_element(By.XPATH, "//div[@class='ellipsis_box_end']")
-        .text
-    )
-    return address
-
-
-def get_address_to(content: str) -> str:
-    address = (
-        content.find_element(
-            By.XPATH,
-            "//td[@class='ant-table-cell ant_table address_max_width to_address']",
-        )
-        .find_element(By.XPATH, "//div[@class='ellipsis_box_start']")
-        .text
-        + content.find_element(
-            By.XPATH,
-            "//td[@class='ant-table-cell ant_table address_max_width to_address']",
-        )
-        .find_element(By.XPATH, "//div[@class='ellipsis_box_end']")
-        .text
-    )
-    return address
-
-
-def get_age(content: str) -> str:
-    age = content.find_element(By.XPATH, "//div[@class='token_black table_pos']").text
-    return age
-
-
-def get_is_outgoing(content: str) -> bool:
-    is_outgoing = (
-        False
-        if content.find_element(By.XPATH, "//div[@class='in-icon to-icon']").text
-        == "In"
-        else True
-    )
-    return is_outgoing
 
 
 def wait_untill_table_is_filled(driver):
@@ -121,110 +66,143 @@ def wait_untill_table_is_filled(driver):
     driver.implicitly_wait(3)
 
 
-transfers: List[Transfer] = []
+def get_transfers(address: str) -> List[Transfer]:
+    transfers: List[Transfer] = []
 
-current_address = starting_address
-url = get_transfers_url(starting_address)
-driver.get(url)
+    current_address = address
+    url = get_transfers_url(starting_address)
+    driver.get(url)
 
-try:
-    wait_untill_table_is_filled(driver)
+    try:
+        wait_untill_table_is_filled(driver)
 
-    # # scrap info
-    # soup = BeautifulSoup(driver.page_source)
+        # unhide small transfers
+        if UNHIDE_SMALL_TRANSACTIONS == True:
+            # check if the hide button has already been disabled
+            checked = driver.find_element(
+                By.XPATH,
+                "//button[@class='ant-switch ant-switch-small hiden-scam-switch tron-mr-8px ant-switch-checked']",
+            )
+            if checked:
+                # toggle the button
+                checked.click()
+                wait_untill_table_is_filled(driver)
 
-    # unhide small transfers
-    if UNHIDE_SMALL_TRANSACTIONS == True:
-        # check if the hide button has already been disabled
-        checked = driver.find_element(
-            By.XPATH,
-            "//button[@class='ant-switch ant-switch-small hiden-scam-switch tron-mr-8px ant-switch-checked']",
+        # total number of transfers for this address
+        total_transfers = int(
+            driver.find_element(By.XPATH, "//div[@class='address-txn tron-mr-4px']")
+            .find_element(By.XPATH, "//span[@class='color101010']")
+            .text
         )
-        if checked:
-            # toggle the button
-            checked.click()
-            wait_untill_table_is_filled(driver)
 
-    # total number of transfers for this address
-    total_transfers = int(
-        driver.find_element(By.XPATH, "//div[@class='address-txn tron-mr-4px']")
-        .find_element(By.XPATH, "//span[@class='color101010']")
-        .text
-    )
-
-    # include transacions if total transfers satisfies the limit
-    if total_transfers < LIMIT_BY_TRANSFER_COUNT:
-        if total_transfers > 0:
-            current_page = 1
-            while True:
-                # extract the currently available transfers in this page's table
-                rows = driver.find_elements(
-                    By.XPATH, "//tr[@class='ant-table-row ant-table-row-level-0']"
-                )
-                for row in rows:
-                    address_parent = current_address
-                    address_from = get_address_from(row)
-                    address_to = get_address_to(row)
-                    amount = float(row.text.replace(",", "").split("\n")[3])
-                    age = get_age(row)
-                    is_outgoing = get_is_outgoing(row)
-
-                    if abs(amount) >= MIN_TRANSFER_AMOUNT:
-                        # only include transfers larger than the limit
-                        transfer = Transfer(
-                            address_parent=address_parent,
-                            address_from=address_from,
-                            address_to=address_to,
-                            amount=abs(amount),
-                            age=age,
-                            is_outgoing=is_outgoing,
+        # include transacions if total transfers satisfies the limit
+        if total_transfers < LIMIT_BY_TRANSFER_COUNT:
+            if total_transfers > 0:
+                current_page = 1
+                while True:
+                    # extract the currently available transfers in this page's table
+                    rows = driver.find_elements(
+                        By.XPATH, "//tr[@class='ant-table-row ant-table-row-level-0']"
+                    )
+                    for row in rows:
+                        address_parent = current_address
+                        address_from = (
+                            row.text.replace(",", "").split("\n")[6]
+                            + row.text.replace(",", "").split("\n")[7]
                         )
-                        transfers.append(transfer)
+                        address_to = (
+                            row.text.replace(",", "").split("\n")[9]
+                            + row.text.replace(",", "").split("\n")[10]
+                        )
+                        amount = float(row.text.replace(",", "").split("\n")[3])
+                        age = row.text.replace(",", "").split("\n")[5]
+                        is_outgoing = row.text.replace(",", "").split("\n")[8] == "Out"
 
-                # next page if exists
-                next_page_lable = driver.find_element(
-                    By.XPATH, "//li[@title='Next Page']"
-                )
-                if next_page_lable.get_attribute("aria-disabled") == "false":
-                    # there is a next page
+                        if abs(amount) >= MIN_TRANSFER_AMOUNT:
+                            # only include transfers larger than the limit
+                            transfer = Transfer(
+                                address_parent=address_parent,
+                                address_from=address_from,
+                                address_to=address_to,
+                                amount=abs(amount),
+                                age=age,
+                                is_outgoing=is_outgoing,
+                            )
+                            transfers.append(transfer)
 
-                    # next_page_button = driver.find_element(
-                    #     By.XPATH, "//button[@class='ant-pagination-item-link']"
-                    # )
-                    next_page_lable.click()
-                    current_page += 1
-                    # wait_untill_table_is_filled(driver)
-                    print(f"Flipping the page to page no {current_page}...")
+                    # next page if exists
+                    next_page_lable = driver.find_element(
+                        By.XPATH, "//li[@title='Next Page']"
+                    )
+                    if next_page_lable.get_attribute("aria-disabled") == "false":
+                        # there is a next page
 
-                    WebDriverWait(driver, 60).until(
-                        EC.presence_of_element_located(
-                            (
-                                By.XPATH,
-                                f"//li[@title='{current_page}' and @class='ant-pagination-item ant-pagination-item-{current_page} ant-pagination-item-active']",
+                        # next_page_button = driver.find_element(
+                        #     By.XPATH, "//button[@class='ant-pagination-item-link']"
+                        # )
+                        next_page_lable.click()
+                        current_page += 1
+                        # wait_untill_table_is_filled(driver)
+                        print(f"Flipping the page to page no {current_page}...")
+
+                        WebDriverWait(driver, 60).until(
+                            EC.presence_of_element_located(
+                                (
+                                    By.XPATH,
+                                    f"//li[@title='{current_page}' and @class='ant-pagination-item ant-pagination-item-{current_page} ant-pagination-item-active']",
+                                )
                             )
                         )
-                    )
 
-                    sleep(3)
-                    driver.implicitly_wait(3)
-                    print("Page flipped.")
+                        sleep(3)
+                        driver.implicitly_wait(3)
+                        print("Page flipped.")
 
-                else:
-                    # there is no next page
-                    break
+                    else:
+                        # there is no next page
+                        break
+
+            else:
+                print(f"No transfers found for address <{current_address}>")
 
         else:
-            print(f"No transfers found for address <{current_address}>")
+            print(
+                f"Address <{current_address}> transfers <{total_transfers}>, exceeds the limit <{LIMIT_BY_TRANSFER_COUNT}>."
+            )
 
-    else:
-        print(
-            f"Address <{current_address}> transfers <{total_transfers}>, exceeds the limit <{LIMIT_BY_TRANSFER_COUNT}>."
-        )
+    finally:
+        driver.quit()
+        return transfers
 
 
-finally:
+def get_rec_transfers(address: str, transfers: List[Transfer], depth=0):
+    new_transfers = get_transfers(address)
+    transfers.extend(new_transfers)
+
+    for nt in new_transfers:
+        if nt.is_outgoing:
+            if nt.address_to not in [t.address_parent for t in transfers]:
+                if depth <= MAX_SCAN_DEPTH:
+                    depth += 1
+                    get_rec_transfers(nt.address_to, transfers, depth)
+                else:
+                    print(f"{nt.address_to} hit MAX_SCAN_DEPTH and won't be included.")
+            else:
+                print(f"No more new transfer to for {nt.address_to}")
+
+    return
+
+
+if __name__ == "__main__":
+    final_set_of_transfers: List[Transfer] = []
+
+    starting_address = "TTVQUuYxDZCnFcmju8k8zNJEaBWiR1z6tM"  # input("Enter the starting tron address: ")
+
+    get_rec_transfers(address=starting_address, transfers=final_set_of_transfers)
+
+    df = pd.DataFrame.from_records(
+        [transfer.__dict__ for transfer in final_set_of_transfers]
+    )
+
     # save trasfers to csv file
-    df = pd.DataFrame.from_records([t.__dict__ for t in transfers])
     df.to_csv("transfers.csv")
-
-    driver.quit()
